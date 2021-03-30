cfd8c6944689f781dc13fe408b1a73ec0b731341
ldex <laurent.duveau+git@gmail.com>
2021-03-30 14:18:19 -0400

    break jour 2

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
@@ -38,10 +39,13 @@ export class ProductListComponent implements OnInit {
   }
 
   onSelect(product: Product) {
-    this.selectedProduct = product;
+  //  this.selectedProduct = product;
+    this.router.navigateByUrl('/products/' + product.id);
   }
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private router: Router) {
 
   }
 
